a39c7d1b888ca3bb44b079e96c4dc7d1e96ef112
Jonathan Sugihara <jonathansugihara@Jonathans-MacBook-Pro.local>
2022-10-10 21:22:40 -0600

    thought controller update

--- server.js
+++ server.js
@@ -4,18 +4,19 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.jsoon());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use(require('.routes'));
+app.use(require('./routes'));
 
-mongoose.connect(process.engMONGODB_URI || 'mongodb://localhost/social-netowork', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
   useFindAndModify: false,
-  useNewUrLParser: true,
+  useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
+// Use this to log mongo queries being executed!
 mongoose.set('debug', true);
 
-app.listen(port, () => console.log(`connected on localhost:${PORT}`));
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
